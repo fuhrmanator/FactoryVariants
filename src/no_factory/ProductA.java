908c96e84b751c61d7169417bcac31806d572c5d
Christopher Fuhrman <fuhrmanator@gmail.com>
2019-03-06 15:32:34 +0100

    change to ProductA (implementation behind factory) signature, changes to one unprotected client

--- src/no_factory/ProductA.java
+++ src/no_factory/ProductA.java
@@ -5,7 +5,7 @@ import simple_factory.IProduct;
 public class ProductA implements IProduct {
     private ProductA() {}
     /* funny constructor */
-    public ProductA(float i) {
-        System.out.println("i=" + i);
+    public ProductA(float i, int j) {
+        System.out.println("i=" + i + " and j=" + j);
     }
 }

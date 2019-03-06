544262f4c9b27f081838e4192c853ee578d58f9a
Christopher Fuhrman <fuhrmanator@gmail.com>
2019-03-06 15:31:04 +0100

    change to ProductA (implementation behind factory) signature, no changes to any clients

--- src/no_factory/ProductA.java
+++ src/no_factory/ProductA.java
@@ -5,7 +5,7 @@ import simple_factory.IProduct;
 public class ProductA implements IProduct {
     private ProductA() {}
     /* funny constructor */
-    public ProductA(int i) {
+    public ProductA(float i) {
         System.out.println("i=" + i);
     }
 }

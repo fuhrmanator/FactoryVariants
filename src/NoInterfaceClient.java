9f09084d6e7831fc63a3bee158c6305b14d9e52c
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2019-05-13 20:17:57 +0200

    Made some more coupling types for tests

---
+++ src/NoInterfaceClient.java
@@ -0,0 +1,9 @@
+import simple_factory.IProduct;
+
+public class NoInterfaceClient {
+
+    public static void main(String[] args) {
+
+        no_factory.ProductA productA = new no_factory.ProductA(1, 2);
+    }
+}

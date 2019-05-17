aa281fd8b8b60a37e8c6201fb31b4368a0cb8d45
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2019-05-17 11:04:20 +0200

    Marking interface for testing

--- src/simple_factory/ProductA.java
+++ src/simple_factory/ProductA.java
@@ -1,6 +1,6 @@
 package simple_factory;
 
-class ProductA implements IProduct {
+class ProductA implements IProduct, MarkerA {
     private ProductA() {}
     /* funny constructor */
     public ProductA(int i) {

61d60611b3ec8f9298f2883af6d5db15119c0ae0
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2019-05-20 14:18:11 +0200

    Small change to test bug in Moose

--- src/ClientNoFactoryMain.java
+++ src/ClientNoFactoryMain.java
@@ -1,3 +1,4 @@
+import no_factory.ProductC;
 import simple_factory.IProduct;
 
 public class ClientNoFactoryMain {
@@ -6,6 +7,6 @@ public class ClientNoFactoryMain {
 
         IProduct productA = new no_factory.ProductA(1, 2);
         IProduct productB = new no_factory.ProductB();
-        IProduct productC = new no_factory.ProductC();
+        IProduct productC = new ProductC();
     }
 }

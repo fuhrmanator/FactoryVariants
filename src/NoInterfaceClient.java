f2b427ce1cd1539367bea7211b68ff80c8c55cd5
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2019-05-20 17:25:22 +0200

    Use imports

--- src/NoInterfaceClient.java
+++ src/NoInterfaceClient.java
@@ -1,9 +1,11 @@
+import no_factory.ProductA;
 import simple_factory.IProduct;
+
 
 public class NoInterfaceClient {
 
     public static void main(String[] args) {
 
-        no_factory.ProductA productA = new no_factory.ProductA(1, 2);
+        ProductA productA = new ProductA(1, 2);
     }
 }
